a9cfe45ab50483eb2ff00198e969e37de2c9282d
omertb <omertugrul@gmail.com>
2020-08-17 12:03:10 +0300

    edit script

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -27,3 +27,31 @@
             });
         }).change();
     });
+
+
+     var $table = $('#networksTable')
+var $button = $('#buttonGetSelections')
+
+       const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+    $(function() {
+    $table.on('click-row.bs.table', function (e, row, $element) {
+        delayedFunction();
+    })
+    })
+
+
+  async function delayedFunction() {
+      await sleep(1);
+                  $table = $('#networksTable')
+            var JSON_Selected = $table.bootstrapTable('getSelections');
+            var i;
+            var text = "";
+            console.log(JSON.stringify(JSON_Selected));
+            for (i = 0; i < JSON_Selected.length; i++) {
+                text += "ID Number is: " + JSON_Selected[i]['rowNumber'] + "<br>"
+            }
+            document.getElementById("demo").innerHTML = text;
+  }
+
