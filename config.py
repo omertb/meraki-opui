ac51cfec6d64bc50e4d382c5bd7dd8025e76bef9
omertb <omertugrul@gmail.com>
2020-08-26 21:43:55 +0300

    config credentials are transferred to os env

---
+++ config.py
@@ -0,0 +1,26 @@
+# default config
+import os
+
+
+class BaseConfig(object):
+    DEBUG = False
+    SECRET_KEY = os.environ['FSECRETKEY']
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}@my-postgres/meraki_operator".format(os.environ['PGCRED'])
+    SECURITY_PASSWORD_SALT = os.environ['PASSWDSALT']
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
