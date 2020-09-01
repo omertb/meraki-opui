d51a27b15da6b4e159c20baafdd0624546b244da
omertb <omertugrul@gmail.com>
2020-09-01 16:57:23 +0300

    user groups page layout

--- project/admin/forms.py
+++ project/admin/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Length
-from project.models import Group
+from project.models import Group, User
 
 
 class GroupMembershipForm(FlaskForm):
     new_group_name = StringField('New Group Name: ', validators=[DataRequired(), Length(max=64)])
     select_group = SelectMultipleField('Select Groups: ', validators=[DataRequired()])
+    select_user = SelectMultipleField('Select Groups: ', validators=[DataRequired()])
 
     def set_choices(self):
         self.select_group.choices = [(group.id, group.name) for group in Group.query.all()]
+        self.select_user.choices = [(user.id, user.name) for user in User.query.all()]
