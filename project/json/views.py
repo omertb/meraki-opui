fd0c7446af2943e9539dfe20be13794f8c58183d
omertb <omertugrul@gmail.com>
2020-09-30 00:25:55 +0300

    logging line in json views

--- project/json/views.py
+++ project/json/views.py
@@ -50,6 +50,9 @@ def negate_user_operator_access():
             db_user.operator = not db_user.operator
             # db_user.groups.append(group) if db_user.operator else db_user.groups.remove(group)
             db.session.add(db_user)
+            log_msg = "User: {} - {}'s operator status is {}".format(current_user.username,
+                                                                     db_user.username, db_user.operator)
+            send_wr_log(log_msg)
         try:
             db.session.commit()
         except:
@@ -71,6 +74,9 @@ def negate_user_admin_access():
             db_user.admin = not db_user.admin
             # db_user.groups.append(group) if db_user.admin else db_user.groups.remove(group)
             db.session.add(db_user)
+            log_msg = "User: {} - {}'s admin status is {}".format(current_user.username,
+                                                                  db_user.username, db_user.admin)
+            send_wr_log(log_msg)
         try:
             db.session.commit()
         except:
@@ -92,7 +98,10 @@ def reset_membership():
                 db_user = User.query.filter_by(username=user['name']).first()
                 db_user.groups.clear()
                 db.session.add(db_user)
-                result.append("User: {} groups have been reset!".format(db_user.username))
+                result.append("User: {} group membership has been reset!".format(db_user.username))
+                log_msg = "User: {} - Group membership for user {} is reset.".format(current_user.username,
+                                                                                     db_user.username)
+                send_wr_log(log_msg)
             try:
                 db.session.commit()
                 result.append("DB Write is successful!")
@@ -131,6 +140,9 @@ def add_user():
             for grp_id in group_list:
                 group = Group.query.get(int(grp_id))
                 user.groups.append(group)
+                log_msg = "User: {} - {} is made member of group: {}".format(current_user.username,
+                                                                             user.username, group.name)
+                send_wr_log(log_msg)
             db.session.add(user)
         try:
             db.session.commit()
@@ -152,6 +164,9 @@ def reset_groups():
             db_group.tags.clear()
             db_group.networks.clear()
             db.session.add(db_group)
+            log_msg = "User: {} - Group: {} tag and network relations are reset.".format(current_user.username,
+                                                                                         db_group.name)
+            send_wr_log(log_msg)
             result.append("Group: {} tag and network relations are reset.\n"
                           "Please \'Update Networks\' in Networks page.".format(group['name']))
         try:
@@ -171,6 +186,8 @@ def delete_groups():
         for group in groups_to_be_deleted:
             db_group = Group.query.filter_by(name=group['name']).first()
             db.session.delete(db_group)
+            log_msg = "User: {} - Group: {} is removed.".format(current_user.username, db_group.name)
+            send_wr_log(log_msg)
             result.append("Group: {} is removed".format(group['name']))
         try:
             db.session.commit()
@@ -193,6 +210,9 @@ def tag_group():
             for tag_id in tag_list:
                 tag = Tag.query.get(int(tag_id))
                 group.tags.append(tag)
+                log_msg = "User: {} - Group: {} is associated with tag: {}.".format(current_user.username,
+                                                                                    group.name, tag.name)
+                send_wr_log(log_msg)
             db.session.add(group)
         try:
             db.session.commit()
@@ -256,6 +276,9 @@ def delete_devices():
                               "it is committed!".format(device['name'], network_name))
             else:
                 db.session.delete(db_device)
+                log_msg = "User: {} - Device: {} is removed from Network: {}.".format(current_user.username,
+                                                                                      device['name'], network_name)
+                send_wr_log(log_msg)
                 result.append("Device: {} is removed from Network: {}!".format(device['name'], network_name))
         try:
             db.session.commit()
@@ -281,11 +304,17 @@ def delete_networks():
                                       "it is committed!".format(device.name, db_network.name))
                     else:
                         db.session.delete(device)
+                        log_msg = "User: {} - Device: {} from Network: {} is deleted.".format(current_user.username,
+                                                                                              device.name,
+                                                                                              db_network.name)
+                        send_wr_log(log_msg)
                         result.append("Device: {} from Network: {} is deleted!".format(device.name, db_network.name))
             if db_network.committed:
                 result.append("Network: {} cannot be deleted; it is committed!".format(db_network.name))
             else:
                 db.session.delete(db_network)
+                log_msg = "User: {} - Network: {} is deleted.".format(current_user.username, db_network.name)
+                send_wr_log(log_msg)
                 result.append("Network: {} is deleted!".format(db_network.name))
         try:
             db.session.commit()
@@ -367,6 +396,9 @@ def commit_networks():
                     network_dict['copyFromNetworkId'] = db_network.copied_from.meraki_id
                 # commit to cloud
                 create_net_res = create_network(network_dict)
+                log_msg = "User: {} - Network: {} is created on Meraki Cloud.".format(current_user.username,
+                                                                                      db_network.name)
+                send_wr_log(log_msg)
                 # write new network meraki id to db
                 db_network.meraki_id = create_net_res['id']
                 db.session.add(db_network)
@@ -374,10 +406,17 @@ def commit_networks():
                 if db_network.template:
                     template_meraki_id = db_network.template.meraki_id
                     bind_template(db_network.meraki_id, template_meraki_id)
+                    log_msg = "User: {} - Network: {} is bound to" \
+                              " template {} on Meraki Cloud.".format(current_user.username, db_network.name,
+                                                                     db_network.template.name)
+                    send_wr_log(log_msg)
                 result.append("Network: {} committed to Meraki Cloud".format(db_network.name))
                 db_network.committed = True
             except ConnectionError:
                 result.append("Meraki Response Error for Network: {}".format(db_network.name))
+                log_msg = "User: {} - Meraki Response Error for Network: {}".format(current_user.username,
+                                                                                    db_network.name)
+                send_wr_log(log_msg)
                 db_network.committed = False
         db.session.commit()
         return jsonify(result)
@@ -409,7 +448,16 @@ def commit_devices():
             response = claim_network_devices(meraki_net_id_list[0], dev_serial_list)
             if response == "success":
                 result.append("Devices are claimed and bound to network")
+                log_msg = "User: {} - Devices: {} are claimed and " \
+                          "bound to network: {} on Meraki Cloud.".format(current_user.username,
+                                                                         ", ".join(dev_serial_list),
+                                                                         db_device.network.name)
+                send_wr_log(log_msg)
             else:
+                log_msg = "User: {} - Error for claiming devices: {}: {}".format(current_user.username,
+                                                                                 ", ".join(dev_serial_list),
+                                                                                 response['errors'])
+                send_wr_log(log_msg)
                 result.extend(response['errors'])
 
             # rename devices and get the model name from response and then update the table.
@@ -423,6 +471,9 @@ def commit_devices():
             db.session.commit()
 
         except ConnectionError:
+            log_msg = "User: {} - Meraki Response Error while claiming devices: {}".format(current_user.username,
+                                                                                           ", ".join(dev_serial_list))
+            send_wr_log(log_msg)
             result.append("Meraki Response Error")
 
         return jsonify(result)
@@ -445,10 +496,17 @@ def rename_devices():
                 rename_response = rename_device_v0(meraki_net_id, serial, device_name)
                 result.append("{} with model {} is renamed to {}".format(db_device.name, rename_response['model'],
                                                                          device_name))
+                log_msg = "User: {} - Device: {} is renamed as {}".format(current_user.username, db_device.name,
+                                                                          device_name)
                 db_device.name = device_name
                 db_device.devmodel = rename_response['model']
                 db.session.add(db_device)
+                send_wr_log(log_msg)
+
             except ConnectionError:
+                log_msg = "User: {} - Meraki response error while renaming device: {}.".format(current_user.username,
+                                                                                               device['name'])
+                send_wr_log(log_msg)
                 result.append("Meraki response error while renaming device: {}".format(device['name']))
         db.session.commit()
     return jsonify(result)
